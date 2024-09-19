38d028b051fd4254fc5c253ee8a3776e8368ea88
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-09-19 20:39:06 +0200

    CreateDishDtoValidator tests

--- Gastronomy.Dtos.Tests/CreateDishDtoValidatorTests.cs
+++ Gastronomy.Dtos.Tests/CreateDishDtoValidatorTests.cs
@@ -25,22 +25,49 @@ public sealed class CreateDishDtoValidatorTests
         _stringLocalizer["TooLongDishCategoryNameErrorMessage"].Returns(new LocalizedString("TooLongDishCategoryNameErrorMessage", "Category name is too long."));
         _stringLocalizer["DishCategoryNameTakenErrorMessage"].Returns(new LocalizedString("DishCategoryNameTakenErrorMessage", "Category name is already taken."));
         _stringLocalizer["NewDishCategoryMustBeNullWhenExistingSelectedErrorMessage"].Returns(new LocalizedString("NewDishCategoryMustBeNullWhenExistingSelectedErrorMessage", "New category name must be null when an existing category is selected."));
+        _stringLocalizer["ExistingDishCategoryMustBeNullWhenNewSelectedErrorMessage"].Returns(new LocalizedString("ExistingDishCategoryMustBeNullWhenNewSelectedErrorMessage", "Existing category must be null when a new category provided."));
         _stringLocalizer["ThisDishCategoryDoesNotExistErrorMessage"].Returns(new LocalizedString("ThisDishCategoryDoesNotExistErrorMessage", "This dish category does not exist."));
+        _stringLocalizer["NullExistingCategoryErrorMessage"].Returns(new LocalizedString("NullExistingCategoryErrorMessage", "Existing category must not be null."));
         _validator = new CreateDishDtoValidator(_stringLocalizer, _validationService);
     }
 
+
     [Fact]
-    public async Task Validate_ShouldReturnNoErrors_ForValidDto()
+    public async Task Validate_ShouldReturnNoErrors_ForValidDtoWithNewCategoryName()
     {
         var dto = new CreateDishDto
         {
             Name = "Valid Dish Name",
             BasePrice = 10m,
             ExistingCategoryId = null,
-            NewCategoryName = "Valid Category"
+            NewCategoryName = "Valid Category",
+            IsNewCategory = true
         };
 
-        var result = await _validator.TestValidateAsync(dto);
+        var validationService = Substitute.For<IDishValidationService>();
+        validationService.IsNameTaken(Arg.Any<string>(), Arg.Any<Guid>()).Returns(Task.FromResult(false)); ;
+        validationService.IsNewCategoryNameTaken(Arg.Any<string>()).Returns(Task.FromResult(false));
+        var validator = new CreateDishDtoValidator(_stringLocalizer, validationService);
+        var result = await validator.TestValidateAsync(dto);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact]
+    public async Task Validate_ShouldReturnNoErrors_ForValidDtoWithExistingCategory()
+    {
+        var dto = new CreateDishDto
+        {
+            Name = "Valid Dish Name",
+            BasePrice = 10m,
+            ExistingCategoryId = Guid.NewGuid(),
+            IsNewCategory = false,
+        };
+
+        var validationService = Substitute.For<IDishValidationService>();
+        validationService.IsNameTaken(Arg.Any<string>(), Arg.Any<Guid>()).Returns(Task.FromResult(false));
+        validationService.DishCategoryExists(Arg.Any<Guid>()).Returns(Task.FromResult(true));
+        var validator = new CreateDishDtoValidator(_stringLocalizer, validationService);
+        var result = await validator.TestValidateAsync(dto);
         result.ShouldNotHaveAnyValidationErrors();
     }
 
@@ -49,7 +76,7 @@ public sealed class CreateDishDtoValidatorTests
     {
         var dto = new CreateDishDto
         {
-            Name = new string('A', 51),
+            Name = new string('A', ValidationConstants.MaxDishNameLength + 1),
             BasePrice = 10m
         };
 
@@ -58,12 +85,14 @@ public sealed class CreateDishDtoValidatorTests
             .WithErrorMessage("Dish name is too long.");
     }
 
-    [Fact]
-    public async Task Validate_ShouldReturnError_WhenNameIsEmpty()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task Validate_ShouldReturnError_WhenNameIsEmptyOrNull(string name)
     {
         var dto = new CreateDishDto
         {
-            Name = "",
+            Name = name,
             BasePrice = 10m
         };
 
@@ -88,13 +117,22 @@ public sealed class CreateDishDtoValidatorTests
             .WithErrorMessage("Dish name is already taken.");
     }
 
-    [Fact]
-    public async Task Validate_ShouldReturnError_WhenPriceIsZero()
+    public static IEnumerable<object[]> GetInvalidPrices()
+    {
+        yield return [0m];
+        yield return [-1m];
+        yield return [-2m];
+        yield return [-10.53m];
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidPrices))]
+    public async Task Validate_ShouldReturnError_WhenPriceIsZeroOrLess(decimal price)
     {
         var dto = new CreateDishDto
         {
             Name = "Valid Dish",
-            BasePrice = 0m
+            BasePrice = price
         };
 
         var result = await _validator.TestValidateAsync(dto);
@@ -102,20 +140,53 @@ public sealed class CreateDishDtoValidatorTests
             .WithErrorMessage("Price must be greater than zero.");
     }
 
-    [Fact]
-    public async Task Validate_ShouldReturnError_WhenNewCategoryNameIsEmpty_AndExistingCategoryIdIsNull()
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    public async Task Validate_ShouldReturnError_WhenNewCategoryNameIsNullOrEmpty_AndIsNewCategoryIsTrue(string newCategoryName)
     {
         var dto = new CreateDishDto
         {
             Name = "Valid Dish",
             BasePrice = 10m,
-            ExistingCategoryId = null,
-            NewCategoryName = ""
+            NewCategoryName = newCategoryName,
+            IsNewCategory = true
         };
 
         var result = await _validator.TestValidateAsync(dto);
         result.ShouldHaveValidationErrorFor(x => x.NewCategoryName)
             .WithErrorMessage("New category name cannot be empty.");
+    }
+
+    [Fact]
+    public async Task Validate_ShouldReturnError_WhenIsNewCategoryIsFalseAndNoExistingCategoryIsGiven()
+    {
+        var dto = new CreateDishDto
+        {
+            Name = "Valid Dish",
+            BasePrice = 10m,
+            IsNewCategory = false
+        };
+
+        var result = await _validator.TestValidateAsync(dto);
+        result.ShouldHaveValidationErrorFor(x => x.ExistingCategoryId)
+            .WithErrorMessage("Existing category must not be null.");
+    }
+
+    [Fact]
+    public async Task ShouldReturnError_WhenNewCategoryNameIsGivenButIsNewCategoryIsFalse()
+    {
+        var dto = new CreateDishDto
+        {
+            Name = "Valid Dish",
+            BasePrice = 10m,
+            IsNewCategory = false,
+            NewCategoryName = "Category name"
+        };
+
+        var result = await _validator.TestValidateAsync(dto);
+        result.ShouldHaveValidationErrorFor(x => x.NewCategoryName)
+            .WithErrorMessage("New category name must be null when an existing category is selected.");
     }
 
     [Fact]
@@ -126,7 +197,8 @@ public sealed class CreateDishDtoValidatorTests
             Name = "Valid Dish",
             BasePrice = 10m,
             ExistingCategoryId = null,
-            NewCategoryName = new string('A', 51)
+            NewCategoryName = new string('A', ValidationConstants.MaxDishCategoryNameLength + 1),
+            IsNewCategory = true
         };
 
         var result = await _validator.TestValidateAsync(dto);
@@ -135,44 +207,45 @@ public sealed class CreateDishDtoValidatorTests
     }
 
     [Fact]
-    public async Task Validate_ShouldReturnError_WhenNewCategoryNameIsTaken()
-    {
-        _validationService.IsNewCategoryNameTaken("Taken Category Name").Returns(Task.FromResult(true));
-
-        var dto = new CreateDishDto
-        {
-            Name = "Valid Dish",
-            BasePrice = 10m,
-            ExistingCategoryId = null,
-            NewCategoryName = "Taken Category Name"
-        };
-
-        var result = await _validator.TestValidateAsync(dto);
-        result.ShouldHaveValidationErrorFor(x => x.NewCategoryName)
-            .WithErrorMessage("Category name is already taken.");
-    }
-
-    [Fact]
-    public async Task Validate_ShouldReturnError_WhenNewCategoryNameIsNotNull_AndExistingCategoryIdIsProvided()
+    public async Task ShouldReturnError_WhenExistingCategoryIsGivenButIsNewCategoryIsTrue()
     {
         var dto = new CreateDishDto
         {
             Name = "Valid Dish",
             BasePrice = 10m,
             ExistingCategoryId = Guid.NewGuid(),
-            NewCategoryName = "New Category"
+            NewCategoryName = "Name",
+            IsNewCategory = true
         };
 
         var result = await _validator.TestValidateAsync(dto);
+        result.ShouldHaveValidationErrorFor(x => x.ExistingCategoryId)
+            .WithErrorMessage("Existing category must be null when a new category provided.");
+    }
+
+    [Fact]
+    public async Task Validate_ShouldReturnError_WhenNewCategoryNameIsTaken()
+    {
+        var dto = new CreateDishDto
+        {
+            Name = "Valid Dish",
+            BasePrice = 10m,
+            ExistingCategoryId = null,
+            NewCategoryName = "Taken Category Name",
+            IsNewCategory = true
+        };
+
+        var validationService = Substitute.For<IDishValidationService>();
+        validationService.IsNewCategoryNameTaken("Taken Category Name").Returns(Task.FromResult(true));
+        var validator = new CreateDishDtoValidator(_stringLocalizer, validationService);
+        var result = await validator.TestValidateAsync(dto);
         result.ShouldHaveValidationErrorFor(x => x.NewCategoryName)
-            .WithErrorMessage("New category name must be null when an existing category is selected.");
+            .WithErrorMessage("Category name is already taken.");
     }
 
     [Fact]
     public async Task Validate_ShouldReturnError_WhenExistingCategoryIdDoesNotExist()
     {
-        _validationService.DishCategoryExists(Arg.Any<Guid>()).Returns(Task.FromResult(false));
-
         var dto = new CreateDishDto
         {
             Name = "Valid Dish",
@@ -180,7 +253,10 @@ public sealed class CreateDishDtoValidatorTests
             ExistingCategoryId = Guid.NewGuid()
         };
 
-        var result = await _validator.TestValidateAsync(dto);
+        var validationService = Substitute.For<IDishValidationService>();
+        validationService.DishCategoryExists(Arg.Any<Guid>()).Returns(Task.FromResult(false));
+        var validator = new CreateDishDtoValidator(_stringLocalizer, validationService);
+        var result = await validator.TestValidateAsync(dto);
         result.ShouldHaveValidationErrorFor(x => x.ExistingCategoryId)
             .WithErrorMessage("This dish category does not exist.");
     }
